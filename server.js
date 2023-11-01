aa3191e127e28767dd0fa4d42665338f3be2f64a
1010varun <varunagarwal10.2@gmail.com>
2023-11-01 23:33:27 +0530

    added basic backend

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/get", (req, res) => {
+  res.json({
+    message: "this is a cicd pipeline setrup",
+    status: 200,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("port is listening to port 5000");
+});
