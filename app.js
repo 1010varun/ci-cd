e4614f066174669f669b4239821e43c472b26274
1010varun <varunagarwal10.2@gmail.com>
2023-11-19 14:30:59 +0530

    another commit

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get("/get", (req, res) => {
   });
 });
 
+app.get("/success", (req, res) => {
+  res.json({
+    message: "this is second try to see cicd",
+    status: 200,
+  })
+})
+
 app.listen(5000, () => {
   console.log("port is listening to port 5000");
 });
